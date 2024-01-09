828045a6a70b4c6d066215be74e6298a168d6e0e
Anne Hochwald <153824442+annehochwald@users.noreply.github.com>
2024-01-09 11:48:01 +0100

    Add files via upload

---
+++ prob_calculator.py
@@ -0,0 +1,34 @@
+import copy
+import random
+# Consider using the modules imported above.
+
+class Hat:  
+    def __init__(self, **kwargs):
+        self.contents = []
+        for key, value in kwargs.items():
+            for i in range(value):
+                self.contents.append(key)
+
+    def draw(self, num_balls):
+        removed = []
+        if num_balls > len(self.contents):
+            return self.contents
+        for i in range(num_balls):
+            random_index = random.randrange(len(self.contents))
+            removed_ball = self.contents.pop(random_index)
+            removed.append(removed_ball)
+        return removed
+      
+
+def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    count = 0
+    for i in range(num_experiments):
+        expected_copy = copy.deepcopy(expected_balls)
+        hat_copy = copy.deepcopy(hat)
+        colors_gotten = hat_copy.draw(num_balls_drawn)
+        for color in colors_gotten:
+            if color in expected_copy:
+                expected_copy[color] -= 1
+        if all(x <= 0 for x in expected_copy.values()):
+            count += 1
+    return count / num_experiments  
